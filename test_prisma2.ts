52e3ec3303ac5b010d6bc05ff84d0c72e394eaee
nithu <nithu@nithu.nithu>
2026-07-28 07:28:29 -0400

    chore: remove scratch files to fix build

--- test_prisma2.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "./src/lib/prisma";
-async function run() {
-  try {
-    await prisma.integration.findFirst({ where: { organizationId: { foo: "bar" } } as any });
-  } catch (err: any) {
-    console.log("MESSAGE:", err.message);
-  }
-}
-run();
