e954c28ceee76f3233e7b2af1efb55897e730ed9
nithu <nithu@nithu.nithu>
2026-07-29 01:18:16 -0400

    chore: remove scratch files

--- read_new_pdf.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync("/home/nithu/Music/lead/Robointech_AI_Growth_Readiness_Report_Template.pdf");
-pdf(dataBuffer).then(function(data) { console.log(data.text); }).catch(e => console.log(e));
--- read_pdf.py
+++
@@ -1,9 +0,0 @@
-import sys
-try:
-    import PyPDF2
-    with open('/home/nithu/Music/lead/Robointech_AI_Growth_Readiness_Report_Template.pdf', 'rb') as f:
-        reader = PyPDF2.PdfReader(f)
-        for page in reader.pages:
-            print(page.extract_text())
-except Exception as e:
-    print(f"Error: {e}")
