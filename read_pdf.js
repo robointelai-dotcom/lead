839ea9c2bb6aac27801b9ed0912541d426dc8c09
nithu <nithu@nithu.nithu>
2026-07-29 02:16:39 -0400

    chore: remove scratch files

--- read_pdf.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync("/home/nithu/Downloads/report-Tribeca Dental Care - AI Growth Readiness Report.pdf");
-pdf(dataBuffer).then(function(data) { console.log(data.text); }).catch(e => console.log(e));
